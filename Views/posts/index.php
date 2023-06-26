3f157afeca4a8eb197db778a7205b804efdcd75a
NicolasDrp <drapier.nicolas07@gmail.com>
2023-06-26 16:20:32 +0200

    style : affichage post en card

--- Views/posts/index.php
+++ Views/posts/index.php
@@ -2,23 +2,21 @@
 
 <a href="../post/create">Ecrire un post</a>
 
-<table class="table">
-    <tr>
-        <th>Utilisateur</th>
-        <th>Contenu</th>
-        <th>Date</th>
-    </tr>
-    <?php foreach ($posts as $post) : ?>
-        <tr>
-            <td><?= $post->getIdUtilisateur() ?></td>
-            <td><?= $post->getContenuPost() ?></td>
-            <td><?= $post->getDatePost() ?></td>
-            <td>
-                <a href="./update/<?= $post->getIdPost() ?>">Modifier</a>
-                <a href="./delete/<?= $post->getIdPost() ?>">Supprimer</a>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-</table>
+<?php foreach ($posts as $post) : ?>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title"><?= $post->getIdUtilisateur() ?></h5>
+            <p class="card-text"><?= $post->getContenuPost() ?></p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><?= $post->getDatePost() ?></li>
+        </ul>
+        <div class="card-body">
+            <a href="./update/<?= $post->getIdPost() ?>">Modifier</a>
+            <a href="./delete/<?= $post->getIdPost() ?>">Supprimer</a>
+        </div>
+    </div>
+
+<?php endforeach; ?>
 
 <?php require_once 'Views/foot.php'; ?>
