c1fd9597df710656f51b4a2c663022644940d51c
NicolasDrp <drapier.nicolas07@gmail.com>
2023-06-26 14:19:10 +0200

    Ajout : dump bdd

---
+++ Controllers/PostController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controllers;
+
+require_once 'Models/PostModel.php';
+
+use App\Models\PostModel;
+
+class PostController{
+    protected $postModel;
+
+    public function __construct(){
+        $this->postModel = new PostModel();
+    }
+}
