fcc6b967b8f9efce648a2b77372aaf2227e8e21d
BenCDA <cardosobenjamin01@gmail.com>
2023-07-06 11:42:40 +0200

    format : ajout de commentaires

--- Views/utilisateur/enregistrer.php
+++ Views/utilisateur/enregistrer.php
@@ -8,60 +8,60 @@ use PHPMailer\PHPMailer\Exception;
 require_once 'vendor/autoload.php';
 
 if (isset($_POST['inscription'])) {
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+  $nom = $_POST['nom'];
+  $prenom = $_POST['prenom'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
 
-    $mail = new PHPMailer(true);
-    
+  $mail = new PHPMailer(true);
 
-    try {
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Activer la sortie de débogage détaillée
 
-        $mail->isSMTP();                                            // Envoi via SMTP
+  try {
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Activer la sortie de débogage détaillée
 
-        $mail->Host       = 'smtp.gmail.com';                     // Définir le serveur SMTP à utiliser
+    $mail->isSMTP();                                            // Envoi via SMTP
 
-        $mail->SMTPAuth   = true;                                   // Activer l'authentification SMTP
+    $mail->Host       = 'smtp.gmail.com';                       // Définir le serveur SMTP à utiliser
 
-        $mail->Username   = 'bentwitter59222@gmail.com';                     // Nom d'utilisateur SMTP
+    $mail->SMTPAuth   = true;                                   // Activer l'authentification SMTP
 
-        $mail->Password   = 'mdpdumailusername';                               // Mot de passe SMTP, il faut le vrai mdp de la boite mail pour que ça fonctionne 
+    $mail->Username   = 'bentwitter59222@gmail.com';            // Nom d'utilisateur SMTP
 
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // Activer le chiffrement SSL/TLS implicite
+    $mail->Password   = 'mdpdumailusername';                    // Mot de passe SMTP, il faut le vrai mdp de la boite mail pour que ça fonctionne 
 
-        $mail->Port       = 465;                                    // Port TCP à utiliser ; utilisez 587 si vous avez défini `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // Activer le chiffrement SSL/TLS implicite
 
-        $mail->setFrom('bentwitter59222@gmail.com', 'bonnefete-teuf.com');
+    $mail->Port       = 465;                                    // Port TCP à utiliser ; utilisez 587 si vous avez défini `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-        $mail->addAddress($email, $nom);     // Ajouter un destinataire
+    $mail->setFrom('bentwitter59222@gmail.com', 'bonnefete-teuf.com');
 
-        $mail->isHTML(true);                                  // Définir le format de l'email sur HTML
+    $mail->addAddress($email, $nom);                            // Ajouter un destinataire
 
-        $codeDeVerification = substr(number_format(time() * rand(), 0, '', ''), 0, 6); // Génère un code aléatoire de 6 chiffres
+    $mail->isHTML(true);                                        // Définir le format de l'email sur HTML
 
-        $mail->Subject = 'Email de verification';
-        $mail->Body = 'Voici votre code de vérification :  <b> ' . $codeDeVerification . ' !</b>';
+    $codeDeVerification = substr(number_format(time() * rand(), 0, '', ''), 0, 6); // Génère un code aléatoire de 6 chiffres
 
-        $mail->send();
+    $mail->Subject = 'Email de verification';
+    $mail->Body = 'Voici votre code de vérification :  <b> ' . $codeDeVerification . ' !</b>';
 
-        echo 'Le message a été envoyé';
-        // 
-        $mdpCrypter = password_hash($password, PASSWORD_DEFAULT);
+    $mail->send();
 
-        $bdd = mysqli_connect('localhost:8889', 'adminbf', 'bfadmin', 'bonnefete');
+    echo 'Le message a été envoyé';
+    // 
+    $mdpCrypter = password_hash($password, PASSWORD_DEFAULT);
 
-        $sql = "INSERT INTO utilisateur (nom_utilisateur, prenom_utilisateur, email_utilisateur, password_utilisateur, code_de_verification) VALUES ('$nom', '$prenom', '$email', '$mdpCrypter', '$codeDeVerification')";
+    $bdd = mysqli_connect('localhost:8889', 'adminbf', 'bfadmin', 'bonnefete');
 
-        mysqli_query($bdd, $sql);
+    $sql = "INSERT INTO utilisateur (nom_utilisateur, prenom_utilisateur, email_utilisateur, password_utilisateur, code_de_verification) VALUES ('$nom', '$prenom', '$email', '$mdpCrypter', '$codeDeVerification')";
 
-        header('Location: emailVerification.php?email=' . $email);
+    mysqli_query($bdd, $sql);
 
-        exit();
-    } catch (Exception $e) {
-        echo "Le message n'a pas pu être envoyé. Erreur de l'expéditeur : {$mail->ErrorInfo}";
-    }
+    header('Location: emailVerification.php?email=' . $email);
+
+    exit();
+  } catch (Exception $e) {
+    echo "Le message n'a pas pu être envoyé. Erreur de l'expéditeur : {$mail->ErrorInfo}";
+  }
 }
 ?>
 
