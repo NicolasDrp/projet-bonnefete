ebead0eea3882ac747b8207de6c4c79e8af840e4
NicolasDrp <drapier.nicolas07@gmail.com>
2023-07-06 13:35:29 +0200

    ajout : .env

--- Database.php
+++ Database.php
@@ -1,15 +1,26 @@
 <?php
+
 namespace App;
 
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 use PDO;
+
 class Database {
     protected $pdo;
 
-    public function __construct(){
-        $this->pdo = new PDO("mysql:host=localhost;dbname=bonnefete","root","Oxymore59220");
+
+    public function __construct() {
+        $this->pdo = new PDO($_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
     }
 
-    public function getPdo(){
+    public function getPdo() {
         return $this->pdo;
     }
 }
